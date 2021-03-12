dcd7779a15b6016f36bc993c7f8ee27f6a128a72
Diego <g0d13@outlook.com>
2021-03-11 19:33:01 -0600

    feat: get all users from db, and get users by role

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SSocial.Data;
 using SSocial.Dtos;
@@ -14,24 +14,53 @@ namespace SSocial.Controllers
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UsersController(DataContext context)
+
+        public UsersController(DataContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
         
         [HttpGet]
-        [Authorize(Roles = Role.Admin)]
         public ActionResult<IEnumerable<UserDetails>> GetUsers()
         {
-            var users = from b in _context.Users
-                select new UserDetails()
+            var usersWithRoles = _userManager.GetUsersInRoleAsync(Role.Supervisor)
+                .Result.Select(c => new UserDetails
                 {
-                    Email = b.Email,
-                    UserId = Guid.Parse(b.Id),
-                    Username = b.UserName,
-                };
-            return users.ToList();
+                    Email = c.Email,
+                    Role = Role.Supervisor,
+                    Username = c.UserName,
+                    UserId = Guid.Parse(c.Id)
+                }).Concat(_userManager.GetUsersInRoleAsync(Role.Mechanic).Result
+                    .Select(c => new UserDetails
+                {
+                    Email = c.Email,
+                    Role = Role.Mechanic,
+                    Username = c.UserName,
+                    UserId = Guid.Parse(c.Id)
+                }));
+
+            return usersWithRoles.ToList();
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public ActionResult<IEnumerable<UserDetails>> GetUsersInRole(string name)
+        {
+            var role = _roleManager.Roles.FirstOrDefault(e => e.Name == name);
+            if( role == null)
+                return new BadRequestObjectResult(new { Message = "Role was not found" });
+            return _userManager.GetUsersInRoleAsync(role.Name).Result.Select(c => new UserDetails
+            {
+                Email = c.Email,
+                Role = role.Name,
+                Username = c.UserName,
+                UserId = Guid.Parse(c.Id)
+            }).ToList();
         }
     }
 }
