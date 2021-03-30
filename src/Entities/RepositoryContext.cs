75b47a20d4d200aca09520e33b251eb1524a5b2d
Diego <g0d13@outlook.com>
2021-03-29 21:13:23 -0600

    change context name

---
+++ src/Entities/RepositoryContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class RepositoryContext:  IdentityDbContext<User, Role, Guid>
+    {
+    public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+        
+    public DbSet<Machine> Machines { get; set; }
+        
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Log> Logs { get; set; }
+        
+    public DbSet<Record> Records { get; set; }
+        
+    public DbSet<Request> Request { get; set; }
+        
+    public DbSet<Repair> Repair { get; set; }
+    }
+}
