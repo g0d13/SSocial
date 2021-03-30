5a2c98883d8cc3c175cd46d32f34864231d11532
Diego <g0d13@outlook.com>
2021-03-29 21:18:13 -0600

    delete mapping configuration

--- src/SSocial/Mappings/MappingProfile.cs
+++ src/SSocial/Mappings/MappingProfile.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Linq;
 using AutoMapper;
-using SSocial.Data;
-using SSocial.Dtos;
-using SSocial.Models;
 
 namespace SSocial.Mappings
 {
@@ -11,29 +8,7 @@ namespace SSocial.Mappings
     {
         public MappingProfile()
         {
-            // Origin destiny
-            CreateMap<Category, Guid>().ConstructUsing(x => x.CategoryId);
 
-            CreateMap<ApplicationUser, UserDetails>().ForMember(u => 
-                u.UserId, opt => 
-                opt.MapFrom(e => e.Id)).ReverseMap();
-
-            // CreateMap<Log, Guid>().ConstructUsing(l => l.LogId);
-
-            #region Request
-            CreateMap<Request, RequestDto>()
-                .ForMember(r => r.Machine, opt =>
-                    opt.MapFrom(ops => ops.Machine.MachineId))
-                .ForMember(r => r.Log,
-                    opt => opt.MapFrom(ops => ops.Log.LogId))
-                .ForMember(r => r.Supervisor,
-                    opt => opt.MapFrom(op => op.Supervisor.Id));
-            CreateMap<RequestDto, Request>()
-                .ForMember(r => r.Log, opt => opt.Ignore())
-                .ForMember(r => r.Supervisor, opt => opt.Ignore())
-                .ForMember(r => r.Machine, opt => opt.Ignore());
-            #endregion
-            
         }
     }
 }
