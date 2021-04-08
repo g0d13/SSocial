a87e9956373899c073b871b8885409ae1c7feaac
Diego <g0d13@outlook.com>
2021-04-08 17:39:05 -0500

    feat: create base for logdto

--- src/Entities/DataTransferObjects/LogDto.cs
+++ src/Entities/DataTransferObjects/LogDto.cs
@@ -1,15 +1,28 @@
 using System;
 using System.Collections.Generic;
+using Entities.Models;
 
 namespace Entities.DataTransferObjects
 {
-    public class LogDto
+    public class LogDto : LogBaseDto
+    {
+        
+        public ICollection<CategoryDto> Categories { get; set; }
+        
+        public UserDetails Mechanic { get; set; }
+    }
+
+    public class LogBaseDto
     {
         public Guid LogId { get; set; }
         public string Name { get; set; }
         
         public string Details { get; set; }
-        
+    }
+    public class LogForCreationDto : LogBaseDto
+    {
+        public Guid LogId { get; set; }
+
         public ICollection<Guid> Categories { get; set; }
         public Guid Mechanic { get; set; }
     }
