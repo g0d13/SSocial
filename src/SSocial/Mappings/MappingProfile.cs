a6a2a5a477b8f6bbdd8f7405d5aa806984918902
Diego <g0d13@outlook.com>
2021-04-17 14:11:42 -0500

    feat: add mappings

--- src/SSocial/Mappings/MappingProfile.cs
+++ src/SSocial/Mappings/MappingProfile.cs
@@ -21,12 +21,22 @@ namespace SSocial.Mappings
             CreateMap<User, UserDetails>().ReverseMap();
             CreateMap<CategoryDto, Category>().ReverseMap();
 
+            CreateMap<Machine, MachineDto>()
+                .ReverseMap();
+
             CreateMap<Log, LogDto>().ReverseMap();
+            CreateMap<LogDto, Log>()
+                .ForMember(m => m.MechanicId, opt => opt.MapFrom(c => c.Mechanic.Id));
+            CreateMap<LogDto, LogForCreationDto>();
             CreateMap<LogForCreationDto, Log>()
                 .ForMember(l => l.Categories, opt => opt.Ignore())
                 .ForMember(e => e.MechanicId, 
                     opt => opt.MapFrom(c => c.Mechanic))
                 .ReverseMap();
+            CreateMap<Log, Guid>().ConstructUsing(m => m.LogId);
+
+            CreateMap<RequestDto, Request>().ReverseMap()
+                .ForMember(m => m.Id, opt => opt.MapFrom(f => f.RequestId));
         }
     }
 }
